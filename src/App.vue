6c469675e2caea71664ad726433e364254ce1874
ChillyLife11 <metsatunyanharut7@gmail.com>
2026-08-24 12:15:54 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ onMounted(() => console.log('test'))
 
 <template>
 	<div class="flex min-h-screen *:w-full">
-		AAAA
+		YOOO
 		
 		<!-- <RouterView v-slot="{ Component }">
 			<Transition :name="transition" mode="out-in">
